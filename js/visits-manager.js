5b500a6c92749ae54c4110500b68f6d014a4456d
TibCVA <thibaut.lacour@cva-france.fr>
2025-01-29 13:50:37 +0100

    Update visits-manager.js

--- js/visits-manager.js
+++ js/visits-manager.js
@@ -1,16 +1,17 @@
 // js/visits-manager.js
-// Stocke les visites en localStorage
 class VisitsManager {
   constructor() {
     this.visitsKey = "mdCVA_visits";
   }
+
   loadVisits() {
     const data = StorageUtil.get(this.visitsKey);
     return data || [];
   }
+
   saveVisit(visitObj) {
-    const all = this.loadVisits();
-    all.push(visitObj);
-    StorageUtil.set(this.visitsKey, all);
+    const visits = this.loadVisits();
+    visits.push(visitObj);
+    StorageUtil.set(this.visitsKey, visits);
   }
 }
